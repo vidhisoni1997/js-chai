96a736bcec92bff5cd3da7bd0cb219920f9f72de
vidhi soni <vidhisoni1997@gmail.com>
2026-02-28 16:55:26 +0000

    variables and datatype

---
+++ basics_01/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14453
+let accountEmail = "vidhisoni1997@gmai.com"
+var accountPassword = "341997"
+accountCity = "jaipur"
+
+//accountId = 2
+
+console.log(accountId);
+console.log([accountEmail,accountId,accountPassword]);
+
---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as newer version 
+
+// alert(3+3) //we are using nodejs ,not browser
+console.log(3+3)
+//code readbility should high 
+
+console.log("Vidhi")
+//number =>2 to pwer 53
+//bigint 
+//string ""
+//boolean true/false
+//null standalone value   null is a object not a data type
+//undefined 
